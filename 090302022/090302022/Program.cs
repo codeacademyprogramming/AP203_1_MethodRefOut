ac6329afd14c05bc141d14f71684a4be5c1c8101
eaghayev <eaghayevv@gmail.com>
2022-03-09 20:46:04 +0400

    project added

---
+++ 090302022/090302022/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _090302022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = Sum(45, 10, 7);
+            Console.WriteLine(res);
+
+            int wantedChar = FindCharIndex("programming", 'l');
+
+            Console.WriteLine(wantedChar);
+
+            Console.WriteLine(Sqrt(35));
+        }
+
+        static int Sum(int num1,int num2,int num3=0)
+        {
+            int result = num1 + num2 + num3;
+            return result;
+        }
+
+        static int FindCharIndex(string str,char chr)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == chr)
+                    return i;
+            }
+            return -1;
+        }
+
+        static int Sqrt(int num)
+        {
+            int index = 0;
+
+            while (index*index<=num)
+            {
+                index++;
+            }
+
+            return index - 1;
+        }
+
+
+
+    }
+}
---
+++ 2. 09032021/2. 09032021/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _2._09032021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+
+            string name = "Hikmet";
+
+            ChangeName(ref name);
+
+            Console.WriteLine(name);
+
+            int[] nums = { 1, 2, 3 };
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("========");
+            SetZero(nums);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+
+            int num;
+
+            Add(out num);
+
+            Console.WriteLine(num);
+
+
+            int num2 = 40;
+            MakeOdd(ref num2);
+
+            Console.WriteLine($"num2: {num2}");
+        }
+
+        static void SetZero(int[] nums)
+        {
+            nums[0] = 0;    
+        }
+
+        static void ChangeName(ref string name)
+        {
+            name+="a";
+        }
+
+        static void Add(out int n)
+        {
+            n = 10;
+        }
+        
+        static void MakeOdd(ref int n)
+        {
+            if (n % 2 == 0)
+                n++;
+        }
+    }
+}
